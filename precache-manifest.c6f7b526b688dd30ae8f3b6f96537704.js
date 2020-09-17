b28414fd5ae8fd5843b60730a82961c7280bc334
Carlos Vallejo <cvallejo.ec@gmail.com>
2020-09-17 17:44:42 -0500

    Updates

---
+++ precache-manifest.c6f7b526b688dd30ae8f3b6f96537704.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65b1f63aebacf0b0be0ca1110442d102",
+    "url": "/image-uploader/index.html"
+  },
+  {
+    "revision": "cbaa299848c1ea513ac3",
+    "url": "/image-uploader/static/css/main.cbd09351.chunk.css"
+  },
+  {
+    "revision": "59ecebabf96d5f77cb46",
+    "url": "/image-uploader/static/js/2.4521c052.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/image-uploader/static/js/2.4521c052.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbaa299848c1ea513ac3",
+    "url": "/image-uploader/static/js/main.806f33a4.chunk.js"
+  },
+  {
+    "revision": "ac49343c8734128f9d48",
+    "url": "/image-uploader/static/js/runtime-main.3ce13f2b.js"
+  },
+  {
+    "revision": "454388aa446290a8c7a9c11062785d21",
+    "url": "/image-uploader/static/media/check.454388aa.svg"
+  },
+  {
+    "revision": "3956556ffb4f516c2bebfdbf10b1b49b",
+    "url": "/image-uploader/static/media/image.3956556f.svg"
+  }
+]);
