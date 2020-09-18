c46e2a07c4a80ef715d3a12341f1d7eecbd9ba82
Carlos Vallejo <cvallejo.ec@gmail.com>
2020-09-18 14:12:33 -0500

    Fix the router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import { Route, Switch } from 'react-router';
 
 import Uploader from './components/Uploader.jsx';
@@ -9,12 +9,12 @@ import Uploaded from './components/Uploaded.jsx';
 function App() {
   return (
     <div className="App container ">
-      <Router>
-        <Switch>
-          <Route exact path="/" component={Uploader} />
-          <Route exact path="/uploading" component={Uploading} />
-          <Route exact path="/uploaded" component={Uploaded} />
-        </Switch>
+      <Router basename="/">
+        {/* <Switch> */}
+        <Route exact path="/" component={Uploader} />
+        <Route exact path="/uploading" component={Uploading} />
+        <Route exact path="/uploaded" component={Uploaded} />
+        {/* </Switch> */}
       </Router>
     </div>
   );
